d9b27fbda96ed84874ee21d6e12d8f26cc2721c4
euguroglu <e.uguroglu@yahoo.com>
2021-11-27 22:38:26 +0300

    Custom error message for unit test

---
+++ src/main/java/com/kafkalibrary/controller/LibraryEventControllerAdvice.java
@@ -0,0 +1,32 @@
+package com.kafkalibrary.controller;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@ControllerAdvice
+@Slf4j
+public class LibraryEventControllerAdvice {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleRequestBody(MethodArgumentNotValidException ex){
+
+        List<FieldError> errorList = Collections.singletonList(ex.getBindingResult().getFieldError());
+        String errorMessage = errorList.stream()
+                .map(fieldError -> fieldError.getField() + " - " + fieldError.getDefaultMessage())
+                .sorted()
+                .collect(Collectors.joining(", "));
+
+        log.info("errorMessage: {} ", errorMessage);
+        return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
+    }
+}
--- src/test/java/com/kafkalibrary/LibraryEventsProducerApplicationUnitTest.java
+++ src/test/java/com/kafkalibrary/LibraryEventsProducerApplicationUnitTest.java
@@ -16,6 +16,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import static org.mockito.ArgumentMatchers.isA;
 import static org.mockito.Mockito.doNothing;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
@@ -53,6 +54,32 @@ public class LibraryEventsProducerApplicationUnitTest {
         .content(json)
         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isCreated());
+    }
+
+    @Test
+    void postLibraryEvent_4xx() throws Exception {
+
+        Book book = Book.builder()
+                .bookId(null)
+                .bookAuthor(null)
+                .bookName("Kafka using Spring Boot")
+                .build();
+
+        LibraryEvent libraryEvent = LibraryEvent.builder()
+                .libraryEventId(null)
+                .book(null)
+                .build();
+
+        String json = objectMapper.writeValueAsString(libraryEvent);
+        doNothing().when(libraryEventProducer).sendLibraryEvent(isA(LibraryEvent.class));
+
+        //expect
+        String expectedErrorMessage = "book - must not be null";
+        mockMvc.perform(post("/v1/libraryevent")
+                .content(json)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().is4xxClientError())
+                .andExpect(content().string(expectedErrorMessage));
     }
 
 }
